35482b75653de3f2cdba945c0db7e6fedfa54422
zeff96 <zeffo1975@gmail.com>
2023-01-31 16:47:47 +0300

    Add card array object to js file.

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ document.querySelectorAll('.nav-link').forEach((link) => link.addEventListener('
   hamburgerMenu.classList.remove('active');
   navMenu.classList.remove('active');
 }));
+
+const sectionEl = document.querySelector('.work-container');
+
+const cardArray = [
+  {
+    title: 'post-stories',
+    img: 'img/Img Placeholder.png"',
+    text: "A daily selection of privately personalized reads; no accounts orsign-ups required. has been the industry's standar dummy text eversince the 1500s, when an unknown printer took a standard dummy text",
+    description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veritatis velit adipisci numquam nulla fugit. In accusamus quis eveniet nesciunt deserunt aspernatur recusandae voluptate similique molestias sed accusantium saepe ullam excepturi aperiam tempora qui iste quisquam, incidunt vitae voluptatum praesentium explicabo quae repellendus? Quas sapiente tenetur ea! Necessitatibus minima labore dolor soluta in voluptatem aspernatur officiis culpa cupiditate quo repudiandae ducimus, aperiam facilis fugit atque quibusdam ut quaerat omnis praesentium id consectetur fugiat reprehenderit unde? Dolore expedita debitis error assumenda aliquam, enim iste eligendi, odio reiciendis eos reprehenderit ab! Enim dolor rem ducimus, libero quaerat dolorum rerum deleniti veniam consectetur!'
+  },{
+    title: 'post-stories',
+    img: 'img/Img Placeholder.png"',
+    text: "A daily selection of privately personalized reads; no accounts orsign-ups required. has been the industry's standar dummy text eversince the 1500s, when an unknown printer took a standard dummy text",
+    description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veritatis velit adipisci numquam nulla fugit. In accusamus quis eveniet nesciunt deserunt aspernatur recusandae voluptate similique molestias sed accusantium saepe ullam excepturi aperiam tempora qui iste quisquam, incidunt vitae voluptatum praesentium explicabo quae repellendus? Quas sapiente tenetur ea! Necessitatibus minima labore dolor soluta in voluptatem aspernatur officiis culpa cupiditate quo repudiandae ducimus, aperiam facilis fugit atque quibusdam ut quaerat omnis praesentium id consectetur fugiat reprehenderit unde? Dolore expedita debitis error assumenda aliquam, enim iste eligendi, odio reiciendis eos reprehenderit ab! Enim dolor rem ducimus, libero quaerat dolorum rerum deleniti veniam consectetur!'
+  },{
+    title: 'post-stories',
+    img: 'img/Img Placeholder.png"',
+    text: "A daily selection of privately personalized reads; no accounts orsign-ups required. has been the industry's standar dummy text eversince the 1500s, when an unknown printer took a standard dummy text",
+    description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veritatis velit adipisci numquam nulla fugit. In accusamus quis eveniet nesciunt deserunt aspernatur recusandae voluptate similique molestias sed accusantium saepe ullam excepturi aperiam tempora qui iste quisquam, incidunt vitae voluptatum praesentium explicabo quae repellendus? Quas sapiente tenetur ea! Necessitatibus minima labore dolor soluta in voluptatem aspernatur officiis culpa cupiditate quo repudiandae ducimus, aperiam facilis fugit atque quibusdam ut quaerat omnis praesentium id consectetur fugiat reprehenderit unde? Dolore expedita debitis error assumenda aliquam, enim iste eligendi, odio reiciendis eos reprehenderit ab! Enim dolor rem ducimus, libero quaerat dolorum rerum deleniti veniam consectetur!'
+  },{
+    title: 'post-stories',
+    img: 'img/Img Placeholder.png"',
+    text: "A daily selection of privately personalized reads; no accounts orsign-ups required. has been the industry's standar dummy text eversince the 1500s, when an unknown printer took a standard dummy text",
+    description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veritatis velit adipisci numquam nulla fugit. In accusamus quis eveniet nesciunt deserunt aspernatur recusandae voluptate similique molestias sed accusantium saepe ullam excepturi aperiam tempora qui iste quisquam, incidunt vitae voluptatum praesentium explicabo quae repellendus? Quas sapiente tenetur ea! Necessitatibus minima labore dolor soluta in voluptatem aspernatur officiis culpa cupiditate quo repudiandae ducimus, aperiam facilis fugit atque quibusdam ut quaerat omnis praesentium id consectetur fugiat reprehenderit unde? Dolore expedita debitis error assumenda aliquam, enim iste eligendi, odio reiciendis eos reprehenderit ab! Enim dolor rem ducimus, libero quaerat dolorum rerum deleniti veniam consectetur!'
+  }
+]
