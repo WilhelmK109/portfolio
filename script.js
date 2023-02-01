3a5f320e01072656f36a01b8b25ce5fa2b886c50
WilhelmK109 <kamulungaw@gmail.com>
2023-02-01 14:58:13 +0200

    Add email validation to contact form

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* ============ Mobile menu =========== */
 const hamburgerMenu = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -10,3 +11,19 @@ document.querySelectorAll('.nav-link').forEach((link) => link.addEventListener('
   hamburgerMenu.classList.remove('active');
   navMenu.classList.remove('active');
 }));
+
+/* ============ Validate contact form =========== */
+const validateForm = document.querySelector('.form');
+const errorMsg = document.querySelector('.msg');
+
+validateForm.addEventListener('submit', (event) => {
+  const emailValue = document.getElementById('email').value;
+  if (emailValue.toLowerCase() !== emailValue) {
+    errorMsg.classList.add('active');
+    errorMsg.innerHTML = 'Not sent. Email address should be in lowercase letters';
+    event.preventDefault();
+  } else {
+    errorMsg.textContent = "";
+    validateForm.submit();
+  }
+});
