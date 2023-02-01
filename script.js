dee4e1bd8c8efbd809346b47d76d9ba8060214e2
WilhelmK109 <kamulungaw@gmail.com>
2023-02-01 13:34:17 +0200

    Add popup window section

--- script.js
+++ script.js
@@ -11,28 +11,85 @@ document.querySelectorAll('.nav-link').forEach((link) => link.addEventListener('
   navMenu.classList.remove('active');
 }));
 
-const sectionEl = document.querySelector('.work-container');
+const workSection = document.querySelector('.work-container');
 
 const cardArray = [
   {
-    title: 'post-stories',
-    img: 'img/Img Placeholder.png"',
-    text: "A daily selection of privately personalized reads; no accounts orsign-ups required. has been the industry's standar dummy text eversince the 1500s, when an unknown printer took a standard dummy text",
-    description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veritatis velit adipisci numquam nulla fugit. In accusamus quis eveniet nesciunt deserunt aspernatur recusandae voluptate similique molestias sed accusantium saepe ullam excepturi aperiam tempora qui iste quisquam, incidunt vitae voluptatum praesentium explicabo quae repellendus? Quas sapiente tenetur ea! Necessitatibus minima labore dolor soluta in voluptatem aspernatur officiis culpa cupiditate quo repudiandae ducimus, aperiam facilis fugit atque quibusdam ut quaerat omnis praesentium id consectetur fugiat reprehenderit unde? Dolore expedita debitis error assumenda aliquam, enim iste eligendi, odio reiciendis eos reprehenderit ab! Enim dolor rem ducimus, libero quaerat dolorum rerum deleniti veniam consectetur!'
-  },{
-    title: 'post-stories',
-    img: 'img/Img Placeholder.png"',
-    text: "A daily selection of privately personalized reads; no accounts orsign-ups required. has been the industry's standar dummy text eversince the 1500s, when an unknown printer took a standard dummy text",
-    description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veritatis velit adipisci numquam nulla fugit. In accusamus quis eveniet nesciunt deserunt aspernatur recusandae voluptate similique molestias sed accusantium saepe ullam excepturi aperiam tempora qui iste quisquam, incidunt vitae voluptatum praesentium explicabo quae repellendus? Quas sapiente tenetur ea! Necessitatibus minima labore dolor soluta in voluptatem aspernatur officiis culpa cupiditate quo repudiandae ducimus, aperiam facilis fugit atque quibusdam ut quaerat omnis praesentium id consectetur fugiat reprehenderit unde? Dolore expedita debitis error assumenda aliquam, enim iste eligendi, odio reiciendis eos reprehenderit ab! Enim dolor rem ducimus, libero quaerat dolorum rerum deleniti veniam consectetur!'
-  },{
-    title: 'post-stories',
-    img: 'img/Img Placeholder.png"',
-    text: "A daily selection of privately personalized reads; no accounts orsign-ups required. has been the industry's standar dummy text eversince the 1500s, when an unknown printer took a standard dummy text",
-    description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veritatis velit adipisci numquam nulla fugit. In accusamus quis eveniet nesciunt deserunt aspernatur recusandae voluptate similique molestias sed accusantium saepe ullam excepturi aperiam tempora qui iste quisquam, incidunt vitae voluptatum praesentium explicabo quae repellendus? Quas sapiente tenetur ea! Necessitatibus minima labore dolor soluta in voluptatem aspernatur officiis culpa cupiditate quo repudiandae ducimus, aperiam facilis fugit atque quibusdam ut quaerat omnis praesentium id consectetur fugiat reprehenderit unde? Dolore expedita debitis error assumenda aliquam, enim iste eligendi, odio reiciendis eos reprehenderit ab! Enim dolor rem ducimus, libero quaerat dolorum rerum deleniti veniam consectetur!'
-  },{
-    title: 'post-stories',
-    img: 'img/Img Placeholder.png"',
-    text: "A daily selection of privately personalized reads; no accounts orsign-ups required. has been the industry's standar dummy text eversince the 1500s, when an unknown printer took a standard dummy text",
-    description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veritatis velit adipisci numquam nulla fugit. In accusamus quis eveniet nesciunt deserunt aspernatur recusandae voluptate similique molestias sed accusantium saepe ullam excepturi aperiam tempora qui iste quisquam, incidunt vitae voluptatum praesentium explicabo quae repellendus? Quas sapiente tenetur ea! Necessitatibus minima labore dolor soluta in voluptatem aspernatur officiis culpa cupiditate quo repudiandae ducimus, aperiam facilis fugit atque quibusdam ut quaerat omnis praesentium id consectetur fugiat reprehenderit unde? Dolore expedita debitis error assumenda aliquam, enim iste eligendi, odio reiciendis eos reprehenderit ab! Enim dolor rem ducimus, libero quaerat dolorum rerum deleniti veniam consectetur!'
+    title: 'Profesional Art Printing Data More',
+    img: "img/Snapshoot-Portfolio.png",
+    text: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",  technologies: ['html', 'css', 'javascript'],
+  },
+  {
+    title: 'Data Dashboard Healthcare',
+    img: "img/Snapshoot-Portfolio.png",
+    text: "A daily selection of privately personalized reads; no accounts orsign-ups required. has been the industry's standard",
+  },
+  {
+    title: 'Website Portfolio',
+    img: "img/Img-1.png",
+    text: "A daily selection of privately personalized reads; no accounts orsign-ups required. has been the industry's standard",
+  },
+  {
+    title: 'Profesional Art Printing Data',
+    img: 'img/Snapshoot-Portfolio.png',
+    text: "A daily selection of privately personalized reads; no accounts orsign-ups required. has been the industry's standard",
+    },
+    {
+    title: 'Profetional Art Printing Data More',
+    img: 'img/Img-1.png',
+    text: "A daily selection of privately personalized reads; no accounts orsign-ups required. has been the industry's standard",
+  },
+  {
+    title: 'Website Portfolio',
+    img: 'img/Snapshoot-Portfolio.png',
+    text: "A daily selection of privately personalized reads; no accounts orsign-ups required. has been the industry's standard",
   }
-]
+];
+
+const tech = `
+<li class="card-tech first-tech">html</li>
+<li class="card-tech">bootstrap</li>
+<li class="card-tech">Ruby</li>
+`
+
+for (const i in cardArray) {
+  const card = document.createElement('div');
+  card.classList.add('card-container')
+  const article = document.createElement('article');
+  article.classList.add('card')
+  const cardBlock = document.createElement('div');
+  cardBlock.classList.add('card-2')
+  const cardImage = document.createElement('div');
+  const h3 = document.createElement('h3');
+  h3.innerHTML = cardArray[i].title;
+  const p = document.createElement('p');
+  p.innerHTML = cardArray[i].text;
+  const ul = document.createElement('ul');
+  ul.classList.add('card-technologies');
+  ul.innerHTML = tech;
+  const buttonDiv = document.createElement('div');
+  buttonDiv.classList.add('see-project-btn-div');
+  buttonDiv.addEventListener('click', function () {
+    
+  });
+
+const seeProjectBtn = document.createElement('button');
+seeProjectBtn.classList.add('btn2')
+seeProjectBtn.textContent = 'See Project';
+seeProjectBtn.addEventListener('onclick', function () {
+
+});
+buttonDiv.appendChild(seeProjectBtn);
+  
+  cardBlock.append(h3, p, ul);
+  article.append(cardBlock, buttonDiv);
+  card.append(article);
+  // article.appendChild(buttonDiv);
+
+  workSection.appendChild(card);
+
+}
+
+document.querySelector('.btn').addEventListener('click', function (){
+  cardArray[0];
+});
